dd7c5f5d18f6d4ed0e765f0384bdc850de924762
kliuverTsk <kliuvertfigueroa@gmail.com>
2025-03-18 13:39:48 -0300

    Corregir rutas de imágenes

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ function App() {
       id: 1,
       titulo: "Mini Rede Social",
       descripcion: "Rede social básica mas totalmente funcional que demonstra minhas habilidades em desenvolvimento web",
-      imagen: "/mi-portfolio/imgs/mini-red-social.jpg",
+      imagen: "/portfolio/imgs/mini-red-social.jpg",
       demo: "https://kliuvertsk.github.io/mini-red-social/#/",
       codigo: "https://github.com/kliuverTsk/mini-red-social" 
     },
@@ -43,7 +43,7 @@ function App() {
       id: 2,
       titulo: "Loja Virtual",
       descripcion: "E-commerce desenvolvido com React",
-      imagen: "/mi-portfolio/imgs/tienda-online.png",
+      imagen: "/portfolio/imgs/tienda-online.png",
       demo: "https://kliuvertsk.github.io/tienda-online/#/",
       codigo: "https://github.com/kliuverTsk/tienda-online"
     },
@@ -51,7 +51,7 @@ function App() {
       id: 3,
       titulo: "App do Clima",
       descripcion: "Painel do clima interativo e dados em tempo real",
-      imagen: "/mi-portfolio/imgs/ver-clima.png",
+      imagen: "/portfolio/imgs/ver-clima.png",
       demo: "https://kliuvertsk.github.io/clima/",
       codigo: "https://github.com/kliuverTsk/clima"
     }
